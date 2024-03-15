f8cc0166242eed1245fd47141bf180bde1eeaf6b
Daniel Vallejo <d.vallejovial@gmail.com>
2024-03-15 13:36:04 +0000

    predictions updates

--- pages/our_model.py
+++ pages/our_model.py
@@ -57,7 +57,11 @@ if uploaded_file is not None:
                 st.metric('Winner horses badly predicted', fp) 
                 st.metric('Loser horses correctly predicted', tn)  
                 st.metric('Loser horses badly predicted', fn)    
-                X_pred_transform
+                
+                if st.button('Show raw data'):
+                    df
+                if st.button('Show processed data'):
+                    X_pred_transform
 
     except Exception as e:
         st.error(f"Error: {e}")
